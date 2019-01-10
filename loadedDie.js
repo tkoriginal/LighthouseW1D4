c903e45a79608a32da1f63fadda7c3404c22e61e
tkorignal <tauqueer.khan@gmail.com>
2019-01-10 17:06:04 +0000

    Completed Loaded die

---
+++ loadedDie.js
@@ -0,0 +1,18 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+  counter = 0
+  return function() {
+    /* your code here */
+  const roll =  list[counter];
+  counter ++;
+  return roll;
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
+
